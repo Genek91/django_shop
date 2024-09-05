7ffea808d011204d8d88e2699193cf3ea05df3f8
Genek91 <newtowngenek@gmail.com>
2024-09-05 13:01:01 +0300

    add/admin

--- shop/products/admin.py
+++ shop/products/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from products.models import Product, Category, Subcategory
+
+admin.site.register(Category)
+admin.site.register(Subcategory)
+admin.site.register(Product)
